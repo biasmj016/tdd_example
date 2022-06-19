4837557fe2f3519dd986f3e7bb05557d6350df05
abcms031 <mjkim031@gmail.com>
2022-06-19 23:56:00 +0900

    feat(ch1) : 다중 통화를 지원하는 money 객체 example 추가

---
+++ src/test/java/com/test/driven/development/ch1/TestDrivenDevelopmentEx1.java
@@ -0,0 +1,38 @@
+package com.test.driven.development.ch1;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestDrivenDevelopmentEx1 {
+
+    private ItemPrice is;
+
+    @BeforeEach
+    void setUp() {
+        is = new ItemPrice();
+    }
+
+    @Test
+    public void 상품가격측정테스트() {
+        String item="apple";
+        int expectedAmount=2000;
+
+        int amount = is.getItem(item);
+        assertEquals(expectedAmount, amount);
+    }
+
+    @Test
+    public void 상품가격측정예외테스트_빈값() {
+        String item="";
+        assertThatThrownBy(()-> is.getItem(item)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void 상품가격측정예외테스트_항목에없는값() {
+        String item="peach";
+        assertThatThrownBy(()-> is.getItem(item)).isInstanceOf(IllegalArgumentException.class);
+    }
+}
