4d6a6de8751fa9053cd98df81cd7c284c14e1eee
abcms031 <mjkim031@gmail.com>
2022-06-30 23:48:00 +0900

    feat : 참조가 남아있지않은 하위 클래스 삭제

--- src/main/java/com/test/driven/development/example5/Fruit.java
+++
@@ -1,9 +0,0 @@
-package com.test.driven.development.example5;
-
-public class Fruit extends PriceTag {
-
-    public Fruit(int amount, String item) {
-        super(amount, item);
-    }
-
-}
--- src/main/java/com/test/driven/development/example5/Vegetable.java
+++
@@ -1,8 +0,0 @@
-package com.test.driven.development.example5;
-
-public class Vegetable extends PriceTag {
-
-    public Vegetable(int amount, String item) {
-        super(amount, item);
-    }
-}
