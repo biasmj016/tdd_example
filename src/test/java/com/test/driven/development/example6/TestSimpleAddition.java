f517ef4207071a5a7436e2ffd114d3f459fc9f45
abcms031 <mjkim031@gmail.com>
2022-07-05 23:52:59 +0900

    refactor : 클래스 이름 변경

--- src/test/java/com/test/driven/development/example6/TestSimpleAddition.java
+++ src/test/java/com/test/driven/development/example6/TestSimpleAddition.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class PriceTagTest {
+class TestSimpleAddition {
 
     @Test
     public void testSimpleAddition() {
