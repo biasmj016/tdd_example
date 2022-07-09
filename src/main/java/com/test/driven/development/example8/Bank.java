ca0fab17ee93f165acb6e0c8920be3def2e53093
abcms031 <mjkim031@gmail.com>
2022-07-09 22:50:25 +0900

    feat : helper class 추가

--- src/main/java/com/test/driven/development/example8/Bank.java
+++ src/main/java/com/test/driven/development/example8/Bank.java
@@ -1,10 +1,22 @@
 package com.test.driven.development.example8;
 
+import java.util.Hashtable;
+
 public class Bank {
+
+    private Hashtable rates = new Hashtable();
+
+    public int rate(String from, String to){
+        if(from.equals(to)) return 1;
+        Integer rate = (Integer) rates.get(new Pair(from, to));
+        return rate.intValue();
+    }
+
+    public void addRate(String from, String to, int rate){
+        rates.put(new Pair(from, to), new Integer(rate));
+    }
+
     public Money reduce(Expression source, String to){
         return source.reduce(this, to);
-    }
-    public int rate(String from, String to){
-        return (from.equals("CHF") && to.equals("USD")) ? 2 : 1;
     }
 }
--- src/test/java/com/test/driven/development/example8/TestReduceMoney.java
+++ src/test/java/com/test/driven/development/example8/TestReduceMoney.java
@@ -1,7 +1,5 @@
 package com.test.driven.development.example8;
 
-import com.test.driven.development.example7.Bank;
-import com.test.driven.development.example7.Money;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -9,10 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class TestReduceMoney {
 
     @Test
-    public void TestReduceMoneyDifferentCurrency() {
-        Bank bank = new Bank();
-        bank.addRate("CHF", "USD", 2);
-        Money result = bank.reduce(Money.franc(2), "USD");
-        assertEquals(Money.dollar(2), result);
+    public void testIdentifyRate() {
+        assertEquals(1, new Bank().rate("USD", "USD"));
     }
 }
