d779c3200af3fe879866a13f5c273afbfe8cec14
abcms031 <mjkim031@gmail.com>
2022-07-13 00:12:25 +0900

    feat : times 테스트 추가

--- src/test/java/com/test/driven/development/example8/TestSumMoney.java
+++ src/test/java/com/test/driven/development/example8/TestSumMoney.java
@@ -1,7 +1,6 @@
 package com.test.driven.development.example8;
 
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class TestSumMoney {
@@ -19,6 +18,22 @@ class TestSumMoney {
         Money result = bank.reduce(sum, "USD");
 
         assertEquals(Money.dollar(15), result);
+
+    }
+
+    @Test
+    public void testSumTimes() {
+        Expression fiveBucks = Money.dollar(5);
+        Expression tenFrancs = Money.franc(10);
+
+        Bank bank = new Bank();
+        bank.addRate("CHF", "USD", 2);
+
+        Expression sum = new Sum(fiveBucks, tenFrancs).times(2);
+
+        Money result = bank.reduce(sum, "USD");
+
+        assertEquals(Money.dollar(20), result);
 
     }
 }
