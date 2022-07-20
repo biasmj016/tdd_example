61862fbd2ad0e7cf47ad2b39b4ce87483113b182
abcms031 <mjkim031@gmail.com>
2022-07-20 23:59:09 +0900

    refactor : StringCalculator class 추가

--- src/main/java/com/test/driven/development/calculator/StringCalculator.java
+++ src/main/java/com/test/driven/development/calculator/StringCalculator.java
@@ -1,12 +1,30 @@
 package com.test.driven.development.calculator;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
 
 public class StringCalculator {
 
-    private ArrayList<Integer> operandList = new ArrayList<>();
-    private ArrayList<String> operatorList = new ArrayList<>();
+    private List<Integer> operandList = new ArrayList<>();
+    private List<String> operatorList = new ArrayList<>();
 
+    public StringCalculator() {
+        Scanner scanner = new Scanner(System.in);
+        String expression = scanner.nextLine();
+        String[] expressions = new CalculatorUtil(expression).split();
+
+
+    }
+    public StringCalculator(Integer[] operands, String[] operators) {
+        this.operandList = Arrays.asList(operands);
+        this.operatorList = Arrays.asList(operators);
+    }
+
+    public int[] filterOperandList(String[] expressions){
+        return Arrays.asList(expressions).stream().mapToInt(Integer::parseInt).toArray();
+    }
 
     public int execute(String expression) {
         return 0;
