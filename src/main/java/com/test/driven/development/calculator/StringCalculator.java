fc0fb276bfc84de83fe571aff85c7eabf8fde45e
abcms031 <mjkim031@gmail.com>
2022-07-23 23:58:25 +0900

    refactor : StringCalculator execute 메소드 개발

--- src/main/java/com/test/driven/development/calculator/StringCalculator.java
+++ src/main/java/com/test/driven/development/calculator/StringCalculator.java
@@ -4,18 +4,22 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class StringCalculator {
 
-    private List<String> expressionList = new ArrayList<>();
+    private List<String> operationList = new ArrayList<>();
+    private List<Integer> operandList = new ArrayList<>();
 
     public StringCalculator() {
         Scanner scanner = new Scanner(System.in);
         String expression = scanner.nextLine();
-        new StringCalculator(new CalculatorUtil(expression).split());
     }
-    public StringCalculator(String[] expressions) {
-        this.expressionList = Arrays.asList(expressions);
+
+    public List<String> separateOperation(String[] expressions){
+        return Arrays.asList(expressions)
+                .stream()
+                .collect(Collectors.toList());
     }
 
     public int execute() {
