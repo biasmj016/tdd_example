3730d0400279d62bbe125f4e4e71a88f0f2e3a2c
abcms031 <mjkim031@gmail.com>
2022-07-28 23:53:36 +0900

    refactor : 문자열계산기 step2 개발중

--- src/main/java/com/test/driven/development/calculator/step2/Orders.java
+++ src/main/java/com/test/driven/development/calculator/step2/Orders.java
@@ -1,9 +1,12 @@
 package com.test.driven.development.calculator.step2;
 
 public class Orders {
-    private int[] expressions;
 
-    public Orders(int[] expressions) {
-        this.expressions = expressions;
+    private int[] orders;
+
+    public Orders(String[] expressions) {
+
     }
+
+
 }
